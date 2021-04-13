7512fea21c6ee3782bb47b16686868bc5b72eb4f
Johannes Fröhlich <jfroehlich@users.noreply.github.com>
2021-04-13 08:38:43 +0200

    Added the bluetooth dependencies and a simple device class.

---
+++ source/device.js
@@ -0,0 +1,93 @@
+import {EventEmitter} from 'events';
+import BluetoothSerialPort from 'node-bluetooth-serial-port';
+
+const DEFAULTS = {
+	deviceMAC: '',
+	maxConnectAttempts: 3,
+	connectionAttemptDelay: 500
+};
+
+export default class Device extends EventEmitter {
+	constructor(settings) {
+		super();
+
+		this.config = Object.assign({}, DEFAULTS, settings);
+		this._connection = null;
+		this._port = new BluetoothSerialPort.BluetoothSerialPort();
+		this._port.on('data', buffer => {
+			console.log('from device:', buffer.toString('hex'));
+		});
+	}
+
+	isConnected() {
+		return this._port.isOpen();
+	}
+
+	async connect(address) {
+		if (this.isConnected()) {
+			console.log("Device is already connected to", this.config.deviceMAC);
+			return;
+		}
+		
+		this.config.deviceMAC = !!adress !== false ? address : this.config.deviceMAC;
+		if (!!this.config.deviceMAC === false) {
+			throw new Error("No device MAC adress to connect to.");
+		}
+
+		let attempts = 0;
+		console.log("connecting to", this.config.deviceMAC);
+		
+		// Log a connection attempt
+		const log = msg => console.log(`[LOCAL]: Connection ${attempts+1}/${this.config.maxConnectAttempts}: ${msg}`);
+
+		// Let's try connecting
+		while (attempts < this.config.maxConnectAttempts) {
+			try {
+				const res = await this._attempt();
+				log(res);
+				this._connection = res;
+				return res
+			} catch (err) {
+				log(err);
+				attempts++;
+				await this._sleep(this.config.connectionAttemptDelay);
+			}
+		}
+		console.log('Giving up. :(')
+	}
+
+	async disconnect() {
+		console.log("disconnecting…");
+		// FIXME Wrong? Needs to wait for an event before returning?
+		this._port.close();
+	}
+
+	async write(buffer) {
+		return new Promise((resolve, reject) => {
+			this._port.write(buffer, (error, bytes) => {
+				return !!error ? reject(error) : resolve(bytes);
+			})
+		});
+	}
+
+	async _attempt() {
+		const self = this;
+		
+
+		return new Promise((resolve, reject) => {
+			// Find the device
+			self._port.findSerialPortChannel(this.config.deviceMAC, channel => {
+				// Connect to the device
+				self._port.connect(this.config.deviceMAC, channel, function() {
+					// We connected, resolve
+					resolve('Connected');
+				}, () => reject('Cannot connect'));
+			}, () => reject('Not found'));
+		});	
+	}
+
+	async _sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+}
+
--- source/main.js
+++ source/main.js
@@ -1,1 +1,15 @@
-console.log('hello');
+import Device from './device.js';
+
+const settings = {
+	device: {
+		maxConnectAttempts: 3,
+		connectionAttemptDelay: 500
+	},
+	service: {
+		hostname: "localhost",
+		port: "8000"
+	}
+}
+
+const device = new Device(settings.device);
+device.connect(address);
