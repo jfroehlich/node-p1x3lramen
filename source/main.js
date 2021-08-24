107d2e867bf2592db39e1521f415973a9945f646
Johannes Fröhlich <jfroehlich@users.noreply.github.com>
2021-08-24 21:21:03 +0200

    Renamed address to deviceMac since it was missleading

--- source/main.js
+++ source/main.js
@@ -21,36 +21,34 @@ const settings = {
 // This closure is needed since we do not run this file as module and
 // therefore would have no async-await.
 (async function () {
-	
 	// Let's asume we call index.js always over node
-	let address = process.argv.lenght === 3 ? process.argv[2] : null;
-
+	let deviceMac = process.argv[2] === undefined ? null : process.argv[2];
 
 	// node-bluetooth-serial-port does not support listing paired devices
 	// on linux – sais the documentation.
-	if (address === null && process.platform === 'linux') {
+	if (deviceMac === null && process.platform === 'linux') {
 		console.log("node index.js <DEVICEMAC>")
 		console.log("Can't list paired devices.");
 		return;
 	}
 
-	if (address === null) {
+	if (deviceMac === null) {
 		// Attempt MAC auto detect
 		const devices = await Connection.pairedDevices();
 		for (let i = 0; i < devices.length; i++) {
 			if (devices[i].name === 'Pixoo') {
-				address = devices[i].address.replace('-', ':');
+				deviceMac = devices[i].address.replace('-', ':');
 				break;
 			} 
 		}
 	}
 
-	if (address === null) {
+	if (deviceMac === null) {
 		console.log("node index.js <DEVICEMAC>")
 		console.log("Can't list paired devices.");
 		return;
 	}
-	settings.connection.deviceMAC = address;
+	settings.connection.deviceMAC = deviceMac;
 	
 	// Let's disconnect properly when the app is done, shall we? Oh, there is some sh** going
 	// on with windows (as usual) let's handle that first.
